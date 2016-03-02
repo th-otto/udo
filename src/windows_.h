580563c7a19eab3a61cc3831c1b6d9d82a4b2b54
Thorsten Otto <thotto@users.sourceforge.net>
2016-03-02 17:02:24 +0100

    win32: embed message catalog into resource

--- src/windows_.h
+++ src/windows_.h
@@ -1,3 +1,4 @@
+
 #ifndef __WINDOWS__H__
 #define __WINDOWS__H__ 1
 
@@ -23,7 +24,16 @@
 #include <cderr.h>
 
 
-#define GetInstance()  ((HINSTANCE)GetModuleHandle(NULL))
+#ifndef UNUSED
+#define UNUSED(x) (void)(x)
+#endif
+#ifndef LOCAL
+#define LOCAL static
+#endif
+#ifndef GLOBAL
+#define GLOBAL
+#endif
+#define GetInstance()  ((HINSTANCE)GetModuleHandleW(NULL))
 
 #undef MAKEWPARAM
 #define MAKEWPARAM(l, h) ((WPARAM)(((WORD)((DWORD_PTR)(l) & 0xffff)) | (((DWORD)((WORD)((DWORD_PTR)(h) & 0xffff)) << 16))))
@@ -31,6 +41,8 @@
 #undef IS_INTRESOURCE
 #define IS_INTRESOURCE(_r) ((((ULONG_PTR)(_r)) >> 16) == 0)
 
+#else
+typedef unsigned short LANGID;
 #endif /* __WIN32__ */
 
 #endif /* __WINDOWS__H__ */
