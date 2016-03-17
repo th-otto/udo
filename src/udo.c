eb7d05d98d987c89e76df29d72e1acf8de37f165
Thorsten Otto <thotto@users.sourceforge.net>
2016-03-17 09:26:40 +0100

    udo: order of jump texts in htmlhelp project was wrong

--- src/udo.c
+++ src/udo.c
@@ -11972,8 +11972,8 @@ LOCAL void save_htmlhelp_project(void)
       hhkname,
       old_outfile.name, outfile.suff, /* default file */
       old_outfile.name, outfile.suff, /* home button file */
-      jump1text,
       aboname,
+      jump1text,
       buttons
       );
 
