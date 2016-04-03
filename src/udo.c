40ef8b71b97d2c1b22f76575d1b029480c35a0f2
Thorsten Otto <thotto@users.sourceforge.net>
2016-04-03 16:38:47 +0200

    udo: do not try to recode ignored lines

--- src/udo.c
+++ src/udo.c
@@ -10124,7 +10124,7 @@ LOCAL _BOOL pass1(const char *datei)
       }
       
       /* Kommentare nicht recoden */
-      if (zeile[0] != '#')
+      if (zeile[0] != '#' && pflag[PASS1].ignore_line == 0)
       {
          recode(zeile, iEncodingSource, iEncodingTarget);
       }
@@ -11465,7 +11465,7 @@ LOCAL _BOOL pass2(const char *datei)
          len--;
       }
 
-      if (zeile[0] != EOS && zeile[0] != '#')
+      if (zeile[0] != EOS && zeile[0] != '#' && pflag[PASS2].ignore_line == 0)
       {
          recode(zeile, iEncodingSource, iEncodingTarget);
       }
