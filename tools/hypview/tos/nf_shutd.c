620e2fe70211c5bdf6b4b92d5d14b65fa2aadf5c
Thorsten Otto <admin@tho-otto.de>
2018-04-06 11:46:46 +0200

    Add mode argument to nf_shutdown

--- tools/hypview/tos/nf_shutd.c
+++ tools/hypview/tos/nf_shutd.c
@@ -2,15 +2,17 @@
 #include <mint/arch/nf_ops.h>
 
 
-void nf_shutdown(void)
+long nf_shutdown(int mode)
 {
 	struct nf_ops *nf_ops;
+	long res = 0;
 
 	if ((nf_ops = nf_init()) != NULL)
 	{
 		long shutdown_id = NF_GET_ID(nf_ops, NF_ID_SHUTDOWN);
 		
 		if (shutdown_id)
-        	nf_ops->call(shutdown_id | 0);
+        	res = nf_ops->call(shutdown_id | mode);
 	}
+	return res;
 }
